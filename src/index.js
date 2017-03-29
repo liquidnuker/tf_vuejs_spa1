50f74a6252a038bebdbcd13d7cb5a291123d5c87
administrator <liquidnuker@users.noreply.github.com>
2017-03-28 19:11:28 -0800

    preparing src/index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Header from "./vue-components/header.vue"
+import Footer from "./vue-components/footer.vue"
+
+// Vue.use(VueRouter)
